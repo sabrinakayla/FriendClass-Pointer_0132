320601d6e9d2f177c54ca239e01543f955d25c87
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-28 18:56:24 +0700

    menambahkan fungsi

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -34,3 +34,12 @@ void siswa::setId(int pId)
 {
     id = pId;
 }
+int main()
+{
+    orang joko;
+    joko.setNama("joko susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
